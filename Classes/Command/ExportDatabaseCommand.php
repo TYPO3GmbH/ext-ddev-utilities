488cdb9b0cc3ddcad349ad3ce1a646000aab78fb
Benjamin Kott <benjamin.kott@outlook.com>
2018-09-18 11:09:06 +0200

    [TASK] Remove obsolete comment

--- Classes/Command/ExportDatabaseCommand.php
+++ Classes/Command/ExportDatabaseCommand.php
@@ -9,19 +9,6 @@
 
 namespace T3G\DdevUtilities\Command;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
