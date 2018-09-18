5357bee5ebe114c00101dedc549a765bb20b71b8
Benjamin Kott <benjamin.kott@outlook.com>
2018-09-18 11:17:35 +0200

    [TASK] More cgl cleanups and removal of obsolete files

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,17 +1,30 @@
 <?php
 
-/**
- * Extension Manager/Repository config file for ext "ddev_utilities".
+/*
+ * This file is part of the package t3g/ddev-utilities.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
  */
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'ddev utilities',
     'description' => 'Utility Extension for intended use with ddev-based setups',
     'category' => 'templates',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'TYPO3 GmbH',
+    'author_email' => 'info@typo3.com',
+    'version' => '0.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-9.5.99',
         ],
         'conflicts' => [
+        ],
+        'suggests' => [
         ],
     ],
     'autoload' => [
@@ -19,12 +32,4 @@ $EM_CONF[$_EXTKEY] = [
             'T3G\\DdevUtilities\\' => 'Classes'
         ],
     ],
-    'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'clearCacheOnLoad' => 1,
-    'author' => 'TYPO3 GmbH',
-    'author_email' => 'info@typo3.com',
-    'author_company' => 'TYPO3 GmbH',
-    'version' => '1.0.0',
 ];
